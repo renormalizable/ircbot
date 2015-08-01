e78e2b8c76d8ab31ba63c23493c13c9abac944b6
non <guage.mail@gmail.com>
2015-08-01 10:18:07 -0700

    add tox support

---
+++ toxclient.py
@@ -0,0 +1,139 @@
+import asyncio
+import importlib
+
+import pytriam
+
+from common import dePrefix, Normalize, splitmessage
+
+
+class ToxClient(pytriam.Messager):
+
+    def __init__(self, loop, tox, config):
+        super().__init__(tox)
+
+        self.loop = loop
+
+        self.nick = self.bot.get('name')
+        self.config = importlib.import_module(config)
+        self.key = self.config.key
+
+        self.lines = {}
+        self.time = 60
+        self.msglimit = 1300
+
+        self.deprefix = dePrefix()
+        self.normalize = Normalize(newline='\n', escape=False, convert=False)
+        self.modules = importlib.import_module('modules')
+
+    def group_get_nick(self, target, peer):
+        return self.core.group_peername(target, peer)
+
+    def get_nick(self, target):
+        return self.core.friend_get_name(target)
+
+    def on(self, name):
+        def on_event(fn):
+            if name not in self.events:
+                self.events[name] = list()
+            self.events[name].append(asyncio.coroutine(fn))
+        return on_event
+
+    def trigger(self, name, arguments):
+        print('trigger', name, arguments)
+        @asyncio.coroutine
+        def task():
+            func = self.events.get(name)
+            if func:
+                coros = [f(self, arguments) for f in func]
+                yield from asyncio.wait(coros)
+
+        self.loop.call_soon(asyncio.async, task())
+
+    @asyncio.coroutine
+    def run(self):
+        self.trigger('tox.init', {})
+        checked = False
+
+        while True:
+            status = self.core.self_get_connection_status()
+
+            if not checked and status:
+                self.trigger('tox.connect', {
+                    'status': "Connected"
+                })
+                checked = True
+
+            if checked and not status:
+                self.trigger('tox.connect', {
+                    'status': "Disconnected"
+                })
+                checked = False
+
+            try:
+                self.core.iterate()
+            except UnicodeDecodeError:
+                print('ignore unicode error')
+            yield from asyncio.sleep(0.01)
+
+    def send(self, command, *, target=0, message=''):
+        if command == 'friend':
+            #super().send(target, message)
+            self.core.friend_send_message(target, message)
+        elif command == 'group':
+            self.core.group_message_send(target, message)
+        else:
+            pass
+
+    def reload(self):
+        self.modules = importlib.reload(self.modules)
+        self.config = importlib.reload(self.config)
+        self.key = self.config.key
+
+    def addlines(self, nick, l):
+        if nick not in self.lines:
+            self.lines[nick] = [l, self.loop.call_later(self.time, lambda: self.lines.pop(nick, None))]
+        else:
+            self.lines[nick][0].extend(l)
+
+    def getlines(self, nick):
+        item = self.lines.pop(nick, None)
+        if item:
+            item[1].cancel()
+            return item[0]
+        else:
+            return []
+
+    def sendm(self, target, message, *, command='PRIVMSG', to='', raw=False, mlimit=0, color=None, **kw):
+        prefix = '' if raw else (to + ': ') if to else ''
+        message = self.normalize(message, **kw)
+        print(repr(message))
+        for (i, m) in enumerate(splitmessage(message.encode('utf-8'), self.msglimit)):
+            if mlimit > 0 and i >= mlimit:
+                self.send(command, target=target, message=prefix + '太多了啦...')
+                break
+            self.send(command, target=target, message=prefix + m.decode('utf-8'))
+
+    def sendl(self, target, line, n, *, llimit=0, **kw):
+        sent = False
+        for (i, m) in enumerate(line):
+            if llimit > 0 and i >= llimit:
+                command = kw.get('command', '')
+                to = kw.get('to', '')
+                prefix = (to + ': ') if to else ''
+                self.send(command, target=target, message=prefix + '太长了啦...')
+                break
+            self.sendm(target, m, **kw)
+            sent = True
+            if n > 0 and i >= (n - 1):
+                break
+        if not sent:
+            raise Exception()
+
+    def sender(self, target, content, *, n=-1, llimit=-1, **kw):
+        if n < 0:
+            self.sendm(target, content, **kw)
+        else:
+            if llimit < 0:
+                self.sendl(target, content, n, **kw)
+            else:
+                self.sendl(target, content, n, llimit=llimit, **kw)
---
+++ toxmain.py
@@ -0,0 +1,82 @@
+import asyncio
+
+import toxclient
+
+loop = asyncio.get_event_loop()
+
+bot = toxclient.ToxClient(loop, 'config.json', 'config')
+
+
+@bot.on('tox.init')
+def init(this ,arguments):
+    print("ID: {}".format(this.core.self_get_address()))
+
+@bot.on('tox.connect')
+def status(this, arguments):
+    print("{} to Tox Network.".format(arguments.get('status')))
+
+@bot.on('friend.status')
+def friend_status(this, arguments):
+    target = arguments.get('target')
+    status = arguments.get('status')
+
+    nick = this.get_nick(target)
+    info = "Online" if status else "Offline"
+
+    print("{} is {}.".format(nick, info))
+
+@bot.on('friend.request')
+def friend_request(this, arguments):
+    print("friend request, {} {}".format(
+        arguments.get('pk'),
+        arguments.get('message')
+    ))
+    this.core.friend_add_norequest(arguments.get('pk'))
+
+@bot.on('group.invite')
+def group_invite(this, arguments):
+    group = this.core.join_groupchat(
+        arguments.get('target'),
+        arguments.get('data')
+    )
+    print("Invited to group {}", group)
+
+@bot.on('friend.message')
+def friend_message(this, arguments):
+    target = arguments.get('target')
+    message = arguments.get('message')
+    nick = this.get_nick(target)
+
+    sender = lambda m, **kw: bot.sender(target, m, command='friend', **kw)
+
+    coros = [f(bot, nick, message, sender) for f in bot.modules.table['commands'].privmsg]
+
+    return (yield from asyncio.wait(coros))
+
+@bot.on('group.message.normal')
+def group_message(this, arguments):
+    target = arguments.get('target')
+    peer = arguments.get('peer')
+    message = arguments.get('message')
+    nick = this.group_get_nick(target, peer)
+
+    if message.startswith('@@ '):
+        message = message[3:]
+    else:
+        return
+
+    (nick, message) = bot.deprefix(nick, message)
+
+    sender = lambda m, **kw: bot.sender(target, m, to=('@@ ' + nick), command='group', **kw)
+
+    coros = [f(bot, nick, message, sender) for f in bot.modules.table['commands'].privmsg]
+
+    return (yield from asyncio.wait(coros))
+
+tasks = [bot.run()]
+
+if __name__ == '__main__':
+    try:
+        loop.run_until_complete(asyncio.wait(tasks))
+    except KeyboardInterrupt:
+        bot.save()
