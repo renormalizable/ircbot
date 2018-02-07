1e998b7fef862033fe00a5dca89f06297b02ed54
non <guage.mail@gmail.com>
2018-02-07 10:36:43 +0000

    improve reload and add status

--- modules/admin/__init__.py
+++ modules/admin/__init__.py
@@ -1,11 +1,12 @@
 import importlib
 
+
 path = 'modules.admin.'
 files = ['bot']
 modules = [importlib.reload(importlib.import_module(path + f)) for f in files]
 
-reload = modules[0].reload
 
 privmsg = [
-    reload,
+    modules[0].reload,
+    modules[0].status,
 ]
