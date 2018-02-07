a89ce6b40434ba9b9e42a066623cad1035c77208
renormalization <guage.mail@gmail.com>
2018-02-07 10:26:18 +0000

    clean up tool

--- modules/commands/tool.py
+++ modules/commands/tool.py
@@ -1,36 +1,25 @@
 import asyncio
-import re
+import itertools
 import json
+import re
 from urllib.parse    import quote_plus, quote, urldefrag
+
+import demjson
+import html5lib
+import lxml.html
 from aiohttp         import ClientSession, ClientRequest
 from aiohttp.helpers import parse_mimetype
+from dicttoxml       import dicttoxml
 from lxml            import etree
-import lxml.html
-import html5lib
-from dicttoxml import dicttoxml
-import demjson
-import itertools
+
 
 demjson.undefined = None
 
+# bad encoding
+# https://item.m.jd.com
 
 def drop(l, offset):
     return itertools.islice(l, offset, None)
-
-#@asyncio.coroutine
-#def fetch(method, url, n, func, send, **kw):
-#    print('fetch')
-#    #r = yield from asyncio.wait_for(request('GET', urldefrag(url)[0], **kw), 1)
-#    r = yield from request(method, urldefrag(url)[0], **kw)
-#    #byte = yield from r.read()
-#    try:
-#        byte = yield from r.text()
-#    except:
-#        print('bad encoding')
-#        byte = yield from r.read()
-#    print('get byte')
-#    l = yield from func(byte)
-#    send(l, n=n, llimit=10)
 
 
 def charset(r):
@@ -39,27 +28,21 @@ def charset(r):
     return params.get('charset')
 
 
-async def fetch(method, url, content='text', **kw):
-    print('fetch')
-    timeout = 10
-    #h = kw.get('headers')
-    #if h:
-    #    c = h.get('Connection')
-    #    if not c:
-    #        h['Connection'] = 'close'
-    #else:
-    #    kw['headers'] = {'Connection': 'close'}
-    #r = yield from asyncio.wait_for(request(method, urldefrag(url)[0], **kw), timeout)
-    #r = yield from request(method, urldefrag(url)[0], **kw)
-    async with ClientSession(request_class=kw.get('request_class', ClientRequest)) as session:
-        # workaround
-        kw_req = kw
-        try:
-            del kw_req['request_class']
-        except:
-            pass
-        r = await asyncio.wait_for(session.request(method, urldefrag(url)[0], **kw_req), timeout)
-        print('get bytes from {}'.format(r.url))
+# here we handle timeout differently
+async def fetch(method, url, content='text', timeout=10, **kw):
+    url_defrag = urldefrag(url)[0]
+    # workaround
+    req_class = kw.get('request_class', ClientRequest)
+    req_kw = kw
+    try:
+        del req_kw['request_class']
+    except KeyError:
+        pass
+
+    async with ClientSession(request_class=req_class) as session:
+        r = await asyncio.wait_for(session.request(method, url_defrag, **req_kw), timeout)
+        print('fetching from {}'.format(r.url))
+
         if content == 'raw':
             return r
         elif content == 'byte':
@@ -70,62 +53,13 @@ async def fetch(method, url, content='text', **kw):
             encoding = charset(r) or 'utf-8'
             text = (await asyncio.wait_for(r.read(), timeout)).decode(encoding, 'replace')
             #try:
-            #    text = yield from r.text()
+            #    text = await asyncio.wait_for(r.text(), timeout)
             #except:
             #    print('bad encoding')
-            #    text = (yield from r.read()).decode('utf-8', 'replace')
+            #    text = (await asyncio.wait_for(r.read(), timeout)).decode('utf-8', 'replace')
             return text
 
         return None
-
-
-def addstyle(e):
-    # br to newline
-    #print(etree.tostring(e))
-    for br in e.xpath('.//br'):
-        br.tail = '\n' + (br.tail or '')
-    for b in e.xpath('.//b'):
-        b.text = '\\x02' + (b.text or '')
-        b.tail = '\\x02' + (b.tail or '')
-    for i in e.xpath('.//i'):
-        i.text = '\\x1d' + (i.text or '')
-        i.tail = '\\x1d' + (i.tail or '')
-    for u in e.xpath('.//u'):
-        u.text = '\\x1f' + (u.text or '')
-        u.tail = '\\x1f' + (u.tail or '')
-    return e
-
-# use html5lib for standard compliance
-def htmlparse(t, encoding=None):
-    return html5lib.parse(t, treebuilder='lxml', namespaceHTMLElements=False)
-    #try:
-    #    return html5lib.parse(t, treebuilder='lxml', namespaceHTMLElements=False, transport_encoding=encoding)
-    #except TypeError:
-    #    return html5lib.parse(t, treebuilder='lxml', namespaceHTMLElements=False)
-def htmlparsefast(t, *, parser=None):
-    return lxml.html.fromstring(t, parser=parser)
-
-
-def htmltostr(t):
-    return addstyle(htmlparse(t)).xpath('string()')
-
-
-def xmlparse(t, encoding=None):
-    parser = etree.XMLParser(recover=True, encoding=encoding)
-    try:
-        return etree.XML(t, parser)
-    except:
-        return etree.XML(t.encode('utf-8'), parser)
-
-
-def jsonparse(t, encoding=None):
-    try:
-        try:
-            return json.loads(t)
-        except TypeError:
-            return json.loads(t.decode(encoding or 'utf-8', 'replace'))
-    except:
-        return demjson.decode(t, encoding=encoding)
 
 
 class Request:
@@ -160,6 +94,12 @@ class Request:
             b"\x10\xFF[\xFE-\xFF]",
         ]
         self.rvalid = re.compile(b"(?:" + b"|".join(illegal) + b")+")
+        self.ns = {'re': 'http://exslt.org/regular-expressions'}
+        # handy
+        self.islice = itertools.islice
+        self.iter_first = lambda i: ''.join(itertools.islice(i, 1))
+        self.iter_list = lambda i: ', '.join(i)
+
 
     def parsefield(self, field):
         if field:
@@ -179,12 +119,15 @@ class Request:
             # check if e is node
             #l = [y for y in [gete(x) for x in e.xpath(f[0], namespaces=ns)] if y.strip()] if hasattr(e, 'xpath') else [e]
             l = [y for y in [str(get(x, f[1]) or '') for x in e.xpath(f[0], namespaces=ns)] if y.strip()] if hasattr(e, 'xpath') else [e]
-            print(l)
+            print('getfield {}'.format(l))
             return f[2].format(', '.join(l)) if l else ''
         return (lambda e: [getf(e, f) for f in field])
 
     def parse(self, text, encoding):
         pass
+
+    def init_ns(self, t):
+        self.ns = {'re': 'http://exslt.org/regular-expressions'}
 
     def get(self, e, f):
         pass
@@ -192,40 +135,41 @@ class Request:
     def format(self, l):
         return map(lambda e: ' '.join(e), l)
 
-    def addns(self, t, ns):
-        pass
+    def get_xpath(self, element, xpath):
+        return element.xpath(xpath, namespaces=self.ns)
 
-    @asyncio.coroutine
-    def fetch(self, method, url, **kw):
-        return (yield from fetch(method, url, content='byte', **kw))
+    def get_field(self, get, element, field):
+        fs = self.get_xpath(element, field[0])
+        fs = (get(f, field[1]) for f in fs)
+        fs = (f for f in fs if f.strip())
+        return field[2](fs)
 
-    @asyncio.coroutine
-    def __call__(self, arg, lines, send, *, method='GET', field=None, transform=None, get=None, preget=None, format=None, **kw):
+    def get_fields(self, get, element, fields):
+        return (self.get_field(get, element, field) for field in fields)
+
+    async def fetch(self, method, url, **kw):
+        return (await fetch(method, url, content='byte', **kw))
+
+    async def __call__(self, arg, lines, send, *, method='GET', field=None, transform=None, get=None, preget=None, format=None, format_new=None, **kw):
         n = int(arg.get('n') or 3)
         offset = int(arg.get('offset') or 0)
-        method = method
-        url = arg.get('url')
-        xpath = arg['xpath']
-        field = field or self.parsefield(arg.get('field'))
-        transform = transform or (lambda l: l)
-        ns = {'re': 'http://exslt.org/regular-expressions'}
+        try:
+            xpath = arg['xpath']
+        except KeyError:
+            raise Exception('xpath is missing')
 
-        print(field)
-
-        getter = get or self.get
-        if preget:
-            get = lambda e, f: getter(preget(e), f)
-        else:
-            get = getter
-        format = format or ((lambda l: map(lambda e: arg['format'].format(*e), l)) if arg.get('format') else self.format)
-
-        # fetch
+        # fetch text
         if lines:
             text = '\n'.join(lines)
             encoding = None
         else:
-            (text, encoding) = yield from self.fetch(method, url, **kw)
-        # parse
+            try:
+                url = arg['url']
+            except KeyError:
+                raise Exception('url is missing')
+            (text, encoding) = await self.fetch(method, url, **kw)
+        # parse text into xml
+        # text -> tree
         try:
             tree = self.parse(text, encoding)
         except ValueError:
@@ -233,66 +177,147 @@ class Request:
             # actually only works with utf-8 encoding
             # https://bpaste.net/show/438a3ef4f0b7
             tree = self.parse(self.rvalid.sub(b'', text), encoding)
-        self.addns(tree, ns)
-        # find
-        l = tree.xpath(xpath, namespaces=ns)
-        #l = drop(transform(l), offset)
-        l = transform(l)
-        # get
-        line = filter(lambda e: any(e), map(self.getfield(get, ns, field), l))
-        line = drop(line, offset)
-        # send
-        send(format(line), n=n, llimit=10)
+        # add xml namespaces to ns
+        self.init_ns(tree)
+        # find elements
+        # tree -> elements
+        elements = tree.xpath(xpath, namespaces=self.ns)
+
+        field = field or self.parsefield(arg.get('field'))
+        # map elements to elements
+        transformer = transform or (lambda es: es)
+        # map element to string
+        getter_old = get or self.get
+        if preget:
+            getter = lambda e, f: getter_old(preget(e), f)
+        else:
+            getter = getter_old
+        # map strings to strings
+        formatter_old = format or ((lambda l: map(lambda e: arg['format'].format(*e), l)) if arg.get('format') else self.format)
+        formatter = (lambda es: format_new(self, es)) if format_new else (lambda es: ([line] for line in formatter_old(filter(lambda e: any(e), map(self.getfield(getter, self.ns, field), transformer(es))))))
+        ## transform elements
+        #elements = transformer(elements)
+        ## get
+        #lines = filter(lambda e: any(e), map(self.getfield(getter, ns, field), elements))
+        ## format lines
+        #lines = formatter_old(lines)
+
+        # format lines
+        # elements -> lines
+        lines = formatter(elements)
+        lines = itertools.chain.from_iterable(itertools.islice(lines, offset, None))
+        # send lines
+        send(lines, n=n, llimit=10)
+
+
+def addstyle(e):
+    # br to newline
+    #print(etree.tostring(e))
+    for br in e.xpath('.//br'):
+        br.tail = '\n' + (br.tail or '')
+    for b in e.xpath('.//b'):
+        b.text = '\\x02' + (b.text or '')
+        b.tail = '\\x02' + (b.tail or '')
+    for i in e.xpath('.//i'):
+        i.text = '\\x1d' + (i.text or '')
+        i.tail = '\\x1d' + (i.tail or '')
+    for u in e.xpath('.//u'):
+        u.text = '\\x1f' + (u.text or '')
+        u.tail = '\\x1f' + (u.tail or '')
+    return e
+
+
+# use html5lib for standard compliance
+def htmlparse(t, encoding=None):
+    return html5lib.parse(t, treebuilder='lxml', namespaceHTMLElements=False)
+    #try:
+    #    return html5lib.parse(t, treebuilder='lxml', namespaceHTMLElements=False, transport_encoding=encoding)
+    #except TypeError:
+    #    return html5lib.parse(t, treebuilder='lxml', namespaceHTMLElements=False)
+def htmlparsefast(t, *, parser=None):
+    return lxml.html.fromstring(t, parser=parser)
+
+
+def htmltostr(t):
+    return addstyle(htmlparse(t)).xpath('string()')
+
+
+def htmlget(e, f):
+    if hasattr(e, 'xpath'):
+        if not f:
+            return addstyle(e).xpath('string()')
+        elif hasattr(e, f):
+            return getattr(e, f, '')
+        else:
+            return e.attrib.get(f, '')
+    else:
+        return str(e)
 
 
 class HTMLRequest(Request):
 
     def parse(self, text, encoding):
         return htmlparse(text, encoding=encoding)
-        #return htmlparse(text.decode(encoding))
 
     def get(self, e, f):
-        if not f:
-            return addstyle(e).xpath('string()')
-        elif hasattr(e, f):
-            return getattr(e, f)
-        else:
-            return e.attrib.get(f)
+        return htmlget(e, f)
 
 html = HTMLRequest()
+
+
+def xmlparse(t, encoding=None):
+    parser = etree.XMLParser(recover=True, encoding=encoding)
+    try:
+        return etree.XML(t, parser)
+    except:
+        return etree.XML(t.encode('utf-8'), parser)
+
+
+def xmlget(e, f):
+    if hasattr(e, 'xpath'):
+        if not f:
+            return htmltostr(e.text)
+        elif hasattr(e, f):
+            return getattr(e, f, '')
+        else:
+            return e.attrib.get(f, '')
+    else:
+        return str(e)
 
 
 class XMLRequest(Request):
 
     def parse(self, text, encoding):
-        #print(text)
         return xmlparse(text, encoding=encoding)
 
-    def get(self, e, f):
-        if not f:
-            return htmltostr(e.text)
-        elif hasattr(e, f):
-            return getattr(e, f)
-        else:
-            return e.attrib.get(f)
-
-    def addns(self, t, ns):
-        ns.update(t.nsmap)
-        xmlns = ns.pop(None, None)
+    def init_ns(self, t):
+        self.ns = {'re': 'http://exslt.org/regular-expressions'}
+        self.ns.update(t.nsmap)
+        xmlns = self.ns.pop(None, None)
         if xmlns:
-            ns['ns'] = xmlns
+            self.ns['ns'] = xmlns
+
+    def get(self, e, f):
+        return xmlget(e, f)
 
 xml = XMLRequest()
+
+
+def jsonparse(t, encoding=None):
+    try:
+        try:
+            return json.loads(t)
+        except TypeError:
+            return json.loads(t.decode(encoding or 'utf-8', 'replace'))
+    except:
+        return demjson.decode(t, encoding=encoding)
 
 
 class JSONRequest(Request):
 
     def parse(self, text, encoding):
-        #print(text)
         j = jsonparse(text, encoding=encoding)
         b = dicttoxml(j, attr_type=False)
-        #print(j)
-        #print(b)
         return xmlparse(b)
 
     def get(self, e, f):
@@ -301,10 +326,7 @@ class JSONRequest(Request):
 jsonxml = JSONRequest()
 
 
-@asyncio.coroutine
-def regex(arg, lines, send, **kw):
-    print('regex')
-
+async def regex(arg, lines, send, **kw):
     n = int(arg.get('n') or 5)
     url = arg.get('url')
 
@@ -313,7 +335,7 @@ def regex(arg, lines, send, **kw):
     else:
         reg = re.compile(arg['regex'])
 
-    text = '\n'.join(lines) if lines else (yield from fetch('GET', url, **kw))
+    text = '\n'.join(lines) if lines else (await fetch('GET', url, **kw))
     line = map(lambda e: ', '.join(e.groups()), reg.finditer(text))
     send(line, n=n, llimit=10)
 
